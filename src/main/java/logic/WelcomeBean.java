cfe747626f93452b217037dd49980c45f006a882
Kengo Mukai <kengo@sample.com>
2018-09-24 14:43:42 +0900

    プロジェクトの初回コミット

---
+++ src/main/java/logic/WelcomeBean.java
@@ -0,0 +1,16 @@
+package logic;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "welcome", eager = true)
+public class WelcomeBean {
+
+    public WelcomeBean() {
+        System.out.println("WelcomeBean instantiated");
+    }
+
+    public String getMessage() {
+        return "I'm alive!";
+    }
+
+}
