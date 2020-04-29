6ff2503e3f612e353d7c467c321f2b716a6b954c
kiriumi <kiriumi.miko@gmail.com>
2020-04-29 09:26:30 +0900

    回復可能なSQL例外はロールバックしないよう改修

--- src/main/java/mode/TestModeTransactional.java
+++ src/main/java/mode/TestModeTransactional.java
@@ -6,6 +6,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.sql.SQLException;
+import java.sql.SQLTransactionRollbackException;
+import java.sql.SQLTransientException;
 
 import javax.interceptor.InterceptorBinding;
 import javax.transaction.Transactional;
@@ -15,8 +17,10 @@ import javax.transaction.Transactional.TxType;
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE })
-@Transactional(value = TxType.REQUIRES_NEW, rollbackOn = SQLException.class)
+@Transactional(value = TxType.REQUIRES_NEW, rollbackOn = SQLException.class, dontRollbackOn = {
+        SQLTransientException.class, SQLTransactionRollbackException.class })
 public @interface TestModeTransactional {
 
     // トランザクションの範囲の参考：https://qiita.com/sengoku/items/69ed0888964dd505d348
+    // SQL例外の種類の参考：http://itdoc.hitachi.co.jp/manuals/3020/3020645640/W4560552.HTM
 }
