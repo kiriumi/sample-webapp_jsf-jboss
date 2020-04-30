18fbd2fc322618cbc320807c94b531607aad5fdc
kiriumi <kiriumi.miko@gmail.com>
2020-04-30 12:43:19 +0900

    @Transactionalをaroundメソッドに付けるよう変更

--- src/main/java/mode/TestModeTransactional.java
+++ src/main/java/mode/TestModeTransactional.java
@@ -5,22 +5,13 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.sql.SQLException;
-import java.sql.SQLTransactionRollbackException;
-import java.sql.SQLTransientException;
 
 import javax.interceptor.InterceptorBinding;
-import javax.transaction.Transactional;
-import javax.transaction.Transactional.TxType;
 
 @Inherited
 @InterceptorBinding
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.TYPE })
-@Transactional(value = TxType.REQUIRES_NEW, rollbackOn = SQLException.class, dontRollbackOn = {
-        SQLTransientException.class, SQLTransactionRollbackException.class })
 public @interface TestModeTransactional {
 
-    // トランザクションの範囲の参考：https://qiita.com/sengoku/items/69ed0888964dd505d348
-    // SQL例外の種類の参考：http://itdoc.hitachi.co.jp/manuals/3020/3020645640/W4560552.HTM
 }
--- src/main/java/mode/TestModeTransactionalInterceptor.java
+++ src/main/java/mode/TestModeTransactionalInterceptor.java
@@ -1,10 +1,16 @@
 package mode;
 
+import java.sql.SQLException;
+import java.sql.SQLTransactionRollbackException;
+import java.sql.SQLTransientException;
+
 import javax.annotation.Priority;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 @Interceptor
 @Priority(Interceptor.Priority.APPLICATION)
@@ -15,7 +21,12 @@ public class TestModeTransactionalInterceptor {
     private TestMode testMode;
 
     @AroundInvoke
+    @Transactional(value = TxType.REQUIRES_NEW, rollbackOn = SQLException.class, dontRollbackOn = {
+            SQLTransientException.class, SQLTransactionRollbackException.class })
     public Object around(final InvocationContext context) throws Exception {
+
+        // トランザクションの範囲の参考：https://qiita.com/sengoku/items/69ed0888964dd505d348
+        // SQL例外の種類の参考：http://itdoc.hitachi.co.jp/manuals/3020/3020645640/W4560552.HTM
 
         Object result = context.proceed();
 
