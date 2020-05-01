24feb071b314a4b3831ed670fd7266eb575d9b78
kiriumi <kiriumi.miko@gmail.com>
2020-05-02 00:59:29 +0900

    externalContextをInjectで取得するよう改修

--- src/main/java/log/ApplicationLogger.java
+++ src/main/java/log/ApplicationLogger.java
@@ -1,17 +1,21 @@
 package log;
 
 import javax.enterprise.context.Dependent;
-import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
+import javax.inject.Inject;
 
 import org.slf4j.MDC;
 
 @Dependent
 public class ApplicationLogger extends BaseLogger {
 
+    @Inject
+    ExternalContext externalContext;
+
     @Override
     protected void putCustomInfo() {
 
-        String contextPath = FacesContext.getCurrentInstance().getExternalContext().getApplicationContextPath();
+        String contextPath = externalContext.getApplicationContextPath();
 
         MDC.put("contextPath", contextPath.substring(1)); // 頭に/があるため
     }
