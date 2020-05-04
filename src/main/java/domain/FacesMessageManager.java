ab5509d138913234b610867806cf7b7d0d2fad36
kiriumi <kiriumi.miko@gmail.com>
2020-05-05 00:53:34 +0900

    画面にメッセージを出力時に、ログも出力するよう改修

--- src/main/java/domain/FacesMessageManager.java
+++ src/main/java/domain/FacesMessageManager.java
@@ -9,7 +9,10 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.validation.constraints.NotBlank;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RequestScoped
+@Slf4j
 public class FacesMessageManager {
 
     @Inject
@@ -17,6 +20,7 @@ public class FacesMessageManager {
 
     public void setMessage(final Severity severity, @NotBlank final String message) {
         facesContext.addMessage(null, new FacesMessage(severity, message, null));
+        log(severity, message);
     }
 
     public void setAppMessageById(final Severity severity, @NotBlank final String messageId, final Object... params) {
@@ -35,6 +39,19 @@ public class FacesMessageManager {
         String rawMessage = resourceBundle.getString(messageId);
         String message = String.format(rawMessage, params);
         facesContext.addMessage(null, new FacesMessage(severity, message, null));
+
+        log(severity, message);
+    }
+
+    private void log(final Severity severity, final String message) {
+
+        if (severity.equals(FacesMessage.SEVERITY_INFO)) {
+            log.info(message);
+        } else if (severity.equals(FacesMessage.SEVERITY_WARN)) {
+            log.warn(message);
+        } else {
+            log.error(message);
+        }
     }
 
 }
