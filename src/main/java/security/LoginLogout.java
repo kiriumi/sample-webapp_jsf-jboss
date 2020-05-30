8b335c972b3d316c3ca7b9d268b7511372563530
kiriumi <kiriumi.miko@gmail.com>
2020-05-30 21:44:36 +0900

    コメントの追加

--- src/main/java/security/LoginLogout.java
+++ src/main/java/security/LoginLogout.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * ログイン／ログアウト時はチェック対象外とするための目印
+ *
+ * @author kengo
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface LoginLogout {
