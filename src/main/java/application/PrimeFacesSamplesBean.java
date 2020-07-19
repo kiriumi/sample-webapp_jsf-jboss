de1e0c42a89653d062089d380dec16e4e3e6f5dd
kiriumi <kiriumi.miko@gmail.com>
2020-07-20 00:17:13 +0900

    カレンダー機能の追加

--- src/main/java/application/PrimeFacesSamplesBean.java
+++ src/main/java/application/PrimeFacesSamplesBean.java
@@ -1,9 +1,10 @@
 package application;
 
+import java.time.LocalDate;
+
 import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
-import javax.validation.constraints.NotBlank;
 
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.file.UploadedFile;
@@ -11,19 +12,28 @@ import org.primefaces.model.file.UploadedFile;
 import domain.UploadFileService;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Model
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class PrimeFacesSamplesBean extends BaseBackingBean {
 
-    @NotBlank
+    @Getter
+    @Setter
     private String inputValue;
 
     @Inject
     private UploadFileService uploadFileService;
 
+    @Getter
+    @Setter
     private UploadedFile uploadedFile;
+
+    @Getter
+    @Setter
+    private LocalDate calendar;
 
     public String actionAfterConfirm() {
         return null;
