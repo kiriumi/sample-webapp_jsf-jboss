2cc4a8ba7003e645e944b9d70df26206ad5c0f5c
kiriumi <kiriumi.miko@gmail.com>
2020-08-01 23:38:21 +0900

    ファセットにJPAを追加し、JPAのメタモデルを自動生成

---
+++ src/main/java/dto/Code_.java
@@ -0,0 +1,15 @@
+package dto;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2020-08-01T23:36:54.580+0900")
+@StaticMetamodel(Code.class)
+public class Code_ {
+	public static volatile SingularAttribute<Code, String> kind;
+	public static volatile SingularAttribute<Code, String> value;
+	public static volatile SingularAttribute<Code, String> label;
+	public static volatile SingularAttribute<Code, String> description;
+	public static volatile SingularAttribute<Code, Integer> sortOrder;
+}
---
+++ src/main/java/dto/Role_.java
@@ -0,0 +1,12 @@
+package dto;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2020-08-01T23:36:54.583+0900")
+@StaticMetamodel(Role.class)
+public class Role_ {
+	public static volatile SingularAttribute<Role, String> emailaddress;
+	public static volatile SingularAttribute<Role, String> role;
+}
