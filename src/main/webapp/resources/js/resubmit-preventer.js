c036e1d89b83d51cb9b30dc0006bd2f7424a9f25
kiriumi <kiriumi.miko@gmail.com>
2020-10-25 22:42:40 +0900

    ダブルクリック抑止のバグ修正

--- src/main/webapp/resources/js/resubmit-preventer.js
+++ src/main/webapp/resources/js/resubmit-preventer.js
@@ -1,7 +1,7 @@
 /**
  * 二重送信防止のため、要素のクリックイベントを無効化し、ローディング中を示す画像を表示する
  */
-$('form').on('submit', function() {
-    $(this).css('pointer-events', 'none');
+document.addEventListener("submit", function() {
+    $('form').css('pointer-events', 'none');
     $('#loading').show();
 });
