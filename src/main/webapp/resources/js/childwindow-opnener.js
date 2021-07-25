a65e7cc043ac0b3641d359589b43690309649b97
kiriumi <kiriumi.miko@gmail.com>
2021-07-25 11:19:47 +0900

    コメントを追加

--- src/main/webapp/resources/js/childwindow-opnener.js
+++ src/main/webapp/resources/js/childwindow-opnener.js
@@ -2,7 +2,7 @@ function openChildWindow(url, windowName, windowFeature) {
 
     var namespace = Math.random().toString(32).substring(2);
     var childWindow =  window.open(url +  '?childTokenNamespace=' + namespace ,windowName , windowFeature);
-    childWindow.focus();
+    childWindow.focus(); // モダンブラウザはfocusが動作しない場合があるので留意すること
 
     return childWindow;
 }
